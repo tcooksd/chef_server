a376dfaf5b58d1ebcd7deb5225d72bf006889ae8
tcooksd <tcooksd@gmail.com>
2014-06-30 17:31:29 -0700

    Update chef script

--- chef-server-deploy.sh
+++ chef-server-deploy.sh
@@ -1,8 +1,8 @@
 !#/bin/bash 
 
-curl -L https://www.opscode.com/chef/install.sh | sudo bash || echo " curl failed " ; exit 
-mkdir -p /var/chef/cache /var/chef/cookbooks/chef-server || echo " unable to make directories " ; exit 
-yum install wget -y  || echo " unable to install wget " ; exit 
+curl -L https://www.opscode.com/chef/install.sh | sudo bash || echo " curl failed "
+mkdir -p /var/chef/cache /var/chef/cookbooks/chef-server || echo " unable to make directories " 
+yum install wget -y  || echo " unable to install wget "  
 mv chef-server/* /var/chef/cookbooks/chef-server/  
 chef-solo -o 'recipe[chef-server::default]' || echo "chef solo failed " 
 
