9f7960ecbdd3664ba1adf52186d75afc7c972f7d
root <root@chef-test-timc.(none)>
2014-07-01 01:02:32 +0100

    test

--- chef-server-deploy.sh
+++ chef-server-deploy.sh
@@ -1,7 +1,9 @@
-curl -L https://www.opscode.com/chef/install.sh | sudo bash
-mkdir -p /var/chef/cache /var/chef/cookbooks/chef-server
-yum install wget -y 
-wget -qO- https://github.com/opscode-cookbooks/chef-server/archive/master.tar.gz | tar xvzC /var/chef/cookbooks/chef-server --strip-components=1
-chef-solo -o 'recipe[chef-server::default]'
+!#/bin/bash 
+
+curl -L https://www.opscode.com/chef/install.sh | sudo bash || echo " curl failed " ; exit 
+mkdir -p /var/chef/cache /var/chef/cookbooks/chef-server || echo " unable to make directories " ; exit 
+yum install wget -y  || echo " unable to install wget " ; exit 
+wget -qO- https://github.com/opscode-cookbooks/chef-server/archive/master.tar.gz | tar xvzC /var/chef/cookbooks/chef-server --strip-components=1 || echo " github pull failed " 
+chef-solo -o 'recipe[chef-server::default]' || echo "chef solo failed " 
 
 
