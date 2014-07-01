116ca70102b68b4b0b2be4718a5ae9d16093e9bd
Tim Cook <tcooksd@gmail.com>
2014-07-01 01:22:34 +0100

    adding chef server cookbook recipe

--- chef-server-deploy.sh
+++ chef-server-deploy.sh
@@ -3,7 +3,7 @@
 curl -L https://www.opscode.com/chef/install.sh | sudo bash || echo " curl failed " ; exit 
 mkdir -p /var/chef/cache /var/chef/cookbooks/chef-server || echo " unable to make directories " ; exit 
 yum install wget -y  || echo " unable to install wget " ; exit 
-wget -qO- https://github.com/opscode-cookbooks/chef-server/archive/master.tar.gz | tar xvzC /var/chef/cookbooks/chef-server --strip-components=1 || echo " github pull failed " 
+mv chef-server/* /var/chef/cookbooks/chef-server/  
 chef-solo -o 'recipe[chef-server::default]' || echo "chef solo failed " 
 
 
